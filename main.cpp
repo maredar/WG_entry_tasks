cd05efafbcb0549a8f3ee39b06b6a8840f18688a
geterman <moivanov@aol.com>
2022-09-30 20:16:55 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+#include "src/first_task/is_even.hpp"
+#include "src/second_task/cyclic_buffer_list.hpp"
+#include "src/second_task/cyclic_buffer_array.hpp"
+#include "src/third_task/merge_sort.hpp"
+
 int main()
 {
 
